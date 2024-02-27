175424321bed94ca80cb9f5bbca6d477970d3b76
TyreceMclean <tyrecemcleanterence@gmail.com>
2024-02-27 14:45:48 -0400

    themeModel

---
+++ flutter/trokanou1/lib/model/theme.dart
@@ -0,0 +1,11 @@
+class Theme {
+  int? idTheme;
+  String? libelle;
+
+  contactMap() {
+    var mapping = Map<String, dynamic>();
+    mapping['idTheme'] = idTheme;
+    mapping['libelle'] = libelle!;
+    return mapping;
+  }
+}
