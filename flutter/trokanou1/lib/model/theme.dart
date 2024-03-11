4fee919403639e25ef76da7d584d728699acd71a
TyreceMclean <tyrecemcleanterence@gmail.com>
2024-03-11 13:25:59 -0400

    COMMIT

--- flutter/trokanou1/lib/model/theme.dart
+++ flutter/trokanou1/lib/model/theme.dart
@@ -9,3 +9,4 @@ class Theme {
     return mapping;
   }
 }
+
--- flutter/trokanou1/lib/pages/accueil.dart
+++ flutter/trokanou1/lib/pages/accueil.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Accueil extends StatefulWidget {
   const Accueil({super.key});
 
@@ -13,10 +14,26 @@ class _AccueilState extends State<Accueil> {
       appBar: AppBar(
         title: const Text('Trokanou'),
       ),
-      body: const Center(
-        child: Text('Bienvenue sur Trokanou'),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: [
+              Text('Ajouter un produit', style: TextStyle(fontSize: 20)),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Informations Generales', style: TextStyle(fontSize: 20)),
+            ],
+          ),
+          Row(
+            children: [
+              Image.asset('assets/image/calendar.png',width: 150, height: 150,),
+            ],
+          ),
+
+        ],
       ),
     );
   }
 }
-
