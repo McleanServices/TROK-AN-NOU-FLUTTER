472efceb33ee6a96bfd6ee7859d1ca7f4088b624
TyreceMclean <tyrecemcleanterence@gmail.com>
2024-03-14 15:34:21 -0400

    commit

---
+++ flutter/trokanou1/lib/pages/usager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Usager extends StatefulWidget {
+  const Usager({super.key});
+
+  @override
+  State<Usager> createState() => _UsagerState();
+}
+
+class _UsagerState extends State<Usager> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
