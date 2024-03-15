39a33a562b6dc54ecd4e8e3d7d642966308fb87b
CYRILLE <971cyohann@gmail.com>
2024-03-15 09:37:40 -0400

    78

--- flutter/trokanou1/lib/database/databaseConnection.dart
+++ flutter/trokanou1/lib/database/databaseConnection.dart
@@ -1,11 +1,11 @@
-import 'package:sqflite/sqflite.dart' show Database, openDatabase;
+import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 class DatabaseConnection {
   Future<Database> setDatabase() async {
     var directory = await getApplicationDocumentsDirectory();
-    var path = join(directory.path, 'trokannou');
+    var path = join(directory.path, 'trokannou.db');
     var database =
         await openDatabase(path, version: 1, onCreate: _createDatabase);
     return database;
@@ -13,7 +13,7 @@ class DatabaseConnection {
 
   Future<void> _createDatabase(Database database, int version) async {
     String sqlUsager =
-        "CREATE TABLE USAGER(idUsager PRIMARY KEY AUTOINCREMENT , image VARCHARD(255); nom VARCHARD(255),prenom VARCHARD(255),adresse VARCHARD(255),numero_tel INT, mail VARCHARD(255), typeUsage VARCHARD(255))";
+        "CREATE TABLE USAGER(idUsager INTEGER PRIMARY KEY AUTOINCREMENT, image VARCHAR(255), nom VARCHAR(255), prenom VARCHAR(255), adresse VARCHAR(255), numero_tel INTEGER, mail VARCHAR(255), typeUsage VARCHAR(255))";
     String sqlTheme = "";
     String sqlMaterialService = "";
     String sql = sqlUsager + sqlTheme + sqlMaterialService;
--- flutter/trokanou1/lib/pages/utilisateur_compte.dart
+++ flutter/trokanou1/lib/pages/utilisateur_compte.dart
@@ -30,7 +30,7 @@ class _AccueilState extends State<Compte> {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => NouvellePage()),
+                MaterialPageRoute(builder: (context) => ListUSAGER()),
               );
             },
           ),
@@ -60,7 +60,8 @@ class _AccueilState extends State<Compte> {
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => ListUSAGER()),
+                          MaterialPageRoute(
+                              builder: (context) => _Nouvellepage2State()),
                         );
                       },
                     ),
@@ -238,170 +239,334 @@ class _ListUSAGERState extends State<ListUSAGER> {
   }
 
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: _Usager.length,
-        itemBuilder: (context, index) {
-          var usager = _Usager[index];
-          return Card(
-            child: ListTile(
-              onTap: null,
-              leading: Column(children: [
-                CircleAvatar(
-                  child: Text(
-                    usager['photo'],
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Scaffold(
+        appBar: AppBar(title: Text("trokanou")),
+        body: ListView.builder(
+            itemCount: _Usager.length,
+            itemBuilder: (context, index) {
+              var usager = _Usager[index];
+              return Card(
+                child: ListTile(
+                  onTap: null,
+                  leading: Column(children: [
+                    CircleAvatar(
+                      child: Text(
+                        usager['photo'],
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ]),
+                  title: Text(
+                    usager['nom'],
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        usager['prenom'] ?? '',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        usager['addresse'] ?? '',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        usager['numero'] ?? '',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        usager['mail'] ?? '',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        usager['typeUsager'] ?? '',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                    ],
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          _deleteFormDialog(context, usager['id']);
+                        },
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ]),
-              title: Text(
-                usager['nom'],
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    usager['prenom'] ?? '',
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                    ),
-                  ),
-                  Text(
-                    usager['addresse'] ?? '',
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                    ),
-                  ),
-                  Text(
-                    usager['numero'] ?? '',
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                    ),
-                  ),
-                  Text(
-                    usager['mail'] ?? '',
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                    ),
-                  ),
-                  Text(
-                    usager['typeUsager'] ?? '',
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                ],
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      _deleteFormDialog(context, usager['id']);
-                    },
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        });
+              );
+            }));
   }
 }
 
+_deleteFormDialog(BuildContext context, idUsager) {
+  return showDialog(
+      context: context,
+      builder: (param) {
+        return AlertDialog(
+          title: const Text(
+            'Confirmer la suppression',
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          actions: [
+            TextButton(
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.white, backgroundColor: Colors.red),
+              onPressed: () async {
+                Repository _repository =
+                    Repository(); // Initialize _repository here
+                var result = await _repository.deleteUsager('USAGER', idUsager);
+                if (result != null) {
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text('Ok'),
+            ),
+            TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  textStyle: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Annuler'))
+          ],
+        );
+      });
+}
 
+class _Nouvellepage2State extends StatefulWidget {
+  const _Nouvellepage2State({super.key});
 
-// class _Nouvellepage2State extends StatefulWidget {
-//   const _Nouvellepage2State({super.key});
+  @override
+  State<_Nouvellepage2State> createState() => __Nouvellepage2StateState();
+}
 
-//   @override
-//   State<_Nouvellepage2State> createState() => __Nouvellepage2StateState();
-// }
+class __Nouvellepage2StateState extends State<_Nouvellepage2State> {
+  @override
+  late Repository _repository;
+  List<Map<String, dynamic>> _Usager = [];
 
-// class __Nouvellepage2StateState extends State<_Nouvellepage2State> {
-//   @override
+  @override
+  void initState() {
+    super.initState();
+    _repository = Repository();
+    _loadUsager();
+  }
 
-//    late Repository _repository;
-//   List<Map<String, dynamic>> _Usager = [];
+  void _loadUsager() async {
+    List<Map<String, dynamic>> usagers = await _repository.readUsager('USAGER');
+    setState(() {
+      _Usager = usagers;
+    });
+  }
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     _repository = Repository();
-//     _loadUsager();
-//   }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nouvelle Page'),
+      ),
+      body: Container(
+          margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          color: Colors.cyan,
+          height: 750,
+          width: 450,
+          child: Column(
+            children: [
+              Container(),
+              Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.asset(
+                    "asset/image/image.png",
+                    height: 75,
+                    width: 75,
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(0, 80, 0, 0),
+              ),
+              Container(
+                child: Text('Nom', style: TextStyle(fontSize: 25)),
+                margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+              ),
+              Container(
+                child: Text('prenom', style: TextStyle(fontSize: 25)),
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              ),
+              Container(
+                child: Text('Téléphone', style: TextStyle(fontSize: 25)),
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              ),
+              Container(
+                child: Text('Adresse', style: TextStyle(fontSize: 25)),
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              ),
+              Container(
+                child: Text('profession', style: TextStyle(fontSize: 25)),
+                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              ),
+            ],
+          )),
+    );
+  }
+}
 
-//   void _loadUsager() async {
-//     List<Map<String, dynamic>> usagers =
-//         await _repository.readUsager('USAGER');
-//     setState(() {
-//       _Usager = usager;
-//     });
-//   }
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Nouvelle Page'),
-//       ),
-//       body: Container(
-//           margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-//           color: Colors.cyan,
-//           height: 750,
-//           width: 450,
-//           child: Column(
-//             children: [
-//               Container(
-                
-//               ),
-//               Container(
-//                 child: ClipRRect(
-//                   borderRadius: BorderRadius.circular(50),
-//                   child: Image.asset(
-//                     "asset/image/image.png",
-//                     height: 75,
-//                     width: 75,
-//                   ),
-//                 ),
-//                 margin: EdgeInsets.fromLTRB(0, 80, 0, 0),
-//               ),
-//               Container(
-//                 child: Text('Nom', style: TextStyle(fontSize: 25)),
-//                 margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
-//               ),
-//               Container(
-//                 child: Text('prenom', style: TextStyle(fontSize: 25)),
-//                 margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-//               ),
-//               Container(
-//                 child: Text('Téléphone', style: TextStyle(fontSize: 25)),
-//                 margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-//               ),
-//               Container(
-//                 child: Text('Adresse', style: TextStyle(fontSize: 25)),
-//                 margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-//               ),
-//               Container(
-//                 child: Text('profession', style: TextStyle(fontSize: 25)),
-//                 margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-//               ),
-//               ButtonBar()
-              
-//             ],
-//           )),
-//     );
-//   }
-// }
+class FormProd extends StatefulWidget {
+  @override
+  _FormProdState createState() => _FormProdState();
+}
+
+class _FormProdState extends State<FormProd> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Center(
+            child: Image(
+              image: AssetImage('assets/image/logo.png'),
+              width: 200,
+              height: 100,
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Text(
+                    "Ajouter un produit",
+                    style: TextStyle(
+                      fontFamily: 'Times New Roman',
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      foreground: Paint()
+                        ..shader = LinearGradient(
+                          colors: <Color>[
+                            Colors.greenAccent,
+                            Colors.blueAccent,
+                            // Add more colors here.
+                          ],
+                        ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 100.0)),
+                    ),
+                  )),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 20, 0, 10),
+            child: Row(
+              children: [
+                Text('Informations Generales',
+                    style: TextStyle(
+                        fontSize: 20, fontFamily: 'bold', color: Colors.black)),
+              ],
+            ),
+          ),
+          Container(
+            width: 350,
+            child: TextField(
+              maxLength: 20,
+              decoration: InputDecoration(
+                border: null,
+                labelText: 'Nom du produit',
+              ),
+            ),
+          ),
+          Container(
+            width: 350,
+            child: TextField(
+              maxLength: 50,
+              decoration: InputDecoration(
+                border: null,
+                labelText: 'Description',
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: Text(
+                  'Choisir le type de produit',
+                  style: TextStyle(
+                      fontSize: 20, fontFamily: 'bold', color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Row(
+              children: [
+                Radio(
+                  value: 1,
+                  groupValue: null,
+                  onChanged: (value) {},
+                ),
+                Text('Service'),
+                Radio(
+                  value: 1,
+                  groupValue: null,
+                  onChanged: (value) {},
+                ),
+                Text('Pret'),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text('Ajouter une photo du produit',
+                    style: TextStyle(
+                        fontSize: 20, fontFamily: 'bold', color: Colors.black)),
+              ),
+            ],
+          ),
+          Center(
+            child: Image(
+              image: AssetImage('assets/image/calendar.png'),
+              width: 200,
+              height: 200,
+            ),
+          ),
+        ],
+      ),
+    )
+
+        // Add your widget tree here
+        );
+  }
+}
