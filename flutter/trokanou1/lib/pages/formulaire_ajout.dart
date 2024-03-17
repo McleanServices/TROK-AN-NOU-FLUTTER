166df25b46067bd3c6b862ff9db64400fdbcd6f7
CYRILLE <971cyohann@gmail.com>
2024-03-17 12:16:46 -0400

    page usager + connexion bdd

---
+++ flutter/trokanou1/lib/pages/formulaire_ajout.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FormUsager extends StatefulWidget {
+  const FormUsager({super.key});
+
+  @override
+  State<FormUsager> createState() => _FormUsagerState();
+}
+
+class _FormUsagerState extends State<FormUsager> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
