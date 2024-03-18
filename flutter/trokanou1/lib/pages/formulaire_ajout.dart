2030add79d37c9962e0b1d95a4e4a2e719c70e05
CYRILLE <971cyohann@gmail.com>
2024-03-18 13:18:42 -0400

    blow

--- flutter/trokanou1/lib/pages/formulaire_ajout.dart
+++ flutter/trokanou1/lib/pages/formulaire_ajout.dart
@@ -11,7 +11,65 @@ class _FormUsagerState extends State<FormUsager> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-    );
+        body: SingleChildScrollView(
+            child: Column(children: <Widget>[
+      Center(
+        child: Image(
+          image: AssetImage('assets/image/logo.png'),
+          width: 200,
+          height: 100,
+        ),
+      ),
+      Container(
+        width: 350,
+        child: TextField(
+          maxLength: 50,
+          decoration: InputDecoration(
+            border: null,
+            labelText: 'Nom :',
+          ),
+        ),
+      ),
+      Container(
+        width: 350,
+        child: TextField(
+          maxLength: 50,
+          decoration: InputDecoration(
+            border: null,
+            labelText: 'Prénom :',
+          ),
+        ),
+      ),
+      Container(
+        width: 350,
+        child: TextField(
+          maxLength: 50,
+          decoration: InputDecoration(
+            border: null,
+            labelText: 'Téléphone :',
+          ),
+        ),
+      ),
+      Container(
+        width: 350,
+        child: TextField(
+          maxLength: 50,
+          decoration: InputDecoration(
+            border: null,
+            labelText: 'Adresse :',
+          ),
+        ),
+      ),
+      Container(
+        width: 350,
+        child: TextField(
+          maxLength: 50,
+          decoration: InputDecoration(
+            border: null,
+            labelText: 'Proffession :',
+          ),
+        ),
+      ),
+    ])));
   }
 }
